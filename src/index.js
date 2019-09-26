f9768243d1783d1db223569a44a306517d16bcfa
Jacob Lam <jlam18@binghamton.edu>
2019-09-26 11:19:37 -0400

    init browser router, redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import appReducer from './Reducers/appReducer'
 
+const store = createStore(appReducer)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store ={store} >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
