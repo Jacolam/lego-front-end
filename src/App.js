5f34391bd75d524ec2ec03b8b230d5516c01e723
Jacob Lam <jlam18@binghamton.edu>
2019-09-26 11:24:02 -0400

    routes for login and signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h>LEGO APP</h>
+      <Switch>
+        <Route path='/login' component={LogInPage}/>
+        <Route path='/signup' component={SignUp}/>
+      </Switch>
     </div>
   );
 }
