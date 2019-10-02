d5d6c33d4d777909313cd293aece6bf28fbe9a30
Jacob Lam <jlam18@binghamton.edu>
2019-10-02 14:05:39 -0400

    form saving state, need to send fetch request

--- src/Components/AddExercise.js
+++ src/Components/AddExercise.js
@@ -1,12 +1,68 @@
 import React from 'react'
 
 class AddExercise extends React.Component{
+
+  state={
+    name:'',
+    reps:'',
+    weight:'',
+    rpe:'',
+
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    },()=>(console.log(this.state)))
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch()
+  }
   render(){
     return(
       <div>
-        <input></input>
-        <input></input>
-        <input></input>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              name='name'
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Reps:
+            <input
+              type="number"
+              value={this.state.value}
+              name='reps'
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Weight:
+            <input
+              type="number"
+              value={this.state.value}
+              name='weight'
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            RPE:
+            <input
+              type="number"
+              value={this.state.value}
+              name='rpe'
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
       </div>
     )
   }
