15f64a8abe6dc3e5464ccddd87dff7e5178ea0d3
Jacob Lam <jlam18@binghamton.edu>
2019-10-02 13:52:40 -0400

    log exercise form

---
+++ src/Components/AddExercise.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class AddExercise extends React.Component{
+  render(){
+    return(
+      <div>
+        <input></input>
+        <input></input>
+        <input></input>
+      </div>
+    )
+  }
+}
+
+export default AddExercise
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from '../Components/NavBar'
+import AddExercise from '../Components/AddExercise'
 
 class Home extends React.Component{
   render(){
     return(
       <div>
         <NavBar />
+        <AddExercise />
         <h>from home page</h>
 
       </div>
